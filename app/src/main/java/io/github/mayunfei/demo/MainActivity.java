9d0cd5b2c256e7d406543a92d79327788374017f
MaYunFei <mayunfei6@gmail.com>
2017-09-27 15:04:19 +0800

    初始化

---
+++ app/src/main/java/io/github/mayunfei/demo/MainActivity.java
@@ -0,0 +1,22 @@
+package io.github.mayunfei.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout.LayoutParams layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
